4346faee27d5b66612b6d2333af6341057af1079
vinay-talari1432 <vinaytalari10@gmail.com>
2026-02-17 07:46:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Welcome to my portfolio ")
+function gosearch(){
+    let text = document.getElementById("search").value.toLocaleLowerCase();
+    if(text == "python"){
+        document.getElementById("python").scrollIntoView();
+       }
+}
+ 
+// java script
+
+const div = document.getElementById("container")
+const buttons = document.getElementsByClassName("btn")
+console.log(div)
+
